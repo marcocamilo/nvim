7603ff585ab1f53a8873574cb42ef2eb9e41d244
MarcoCamilo <mac.pietri10@gmail.com>
2023-10-20 21:36:55 +0200

    alpha improvement (shortcuts and plugin load time)

--- lua/marco/plugins/alpha.lua
+++ lua/marco/plugins/alpha.lua
@@ -17,22 +17,34 @@ return {
 
 		-- Set menu
 		dashboard.section.buttons.val = {
-			dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-			dashboard.button("e", " " .. " New file", ":ene <BAR> startinsert <CR>"),
-			dashboard.button(
-				"p",
-				" " .. " Find project",
-				":lua require('telescope').extensions.projects.projects()<CR>"
-			),
-			dashboard.button("r", "" .. " Recent files", ":Telescope oldfiles <CR>"),
-			dashboard.button("t", " " .. " Find text", ":Telescope live_grep <CR>"),
-			dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
-			dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+      dashboard.button("n", " " .. " New file",        "<cmd> ene <BAR> startinsert <cr>"),
+      dashboard.button("f", " " .. " Find file",       "<cmd> Telescope find_files <cr>"),
+      dashboard.button("r", " " .. " Recent files",    "<cmd> Telescope oldfiles <cr>"),
+      dashboard.button("t", " " .. " Find text",       "<cmd> Telescope live_grep <cr>"),
+      dashboard.button("c", " " .. " Config",          ":e $MYVIMRC  <cr>"),
+      dashboard.button("l", "󰒲 " .. " Lazy",            "<cmd> Lazy <cr>"),
+      dashboard.button("q", " " .. " Quit",            "<cmd> qa <cr>"),
 		}
 
 		-- Send config to alpha
 		alpha.setup(dashboard.opts)
 
+    vim.api.nvim_create_autocmd("User", {
+      once = true,
+      pattern = "LazyVimStarted",
+      callback = function()
+        local stats = require("lazy").stats()
+        local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+        dashboard.section.footer.val = "⚡ Neovim loaded "
+          .. stats.loaded
+          .. "/"
+          .. stats.count
+          .. " plugins in "
+          .. ms
+          .. "ms"
+        pcall(vim.cmd.AlphaRedraw)
+      end,
+    })
 		-- Disable folding on alpha buffer
 		vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
 	end,
