319994f0f0bc2400b6a381277aa7aa6ce80ee517
MarcoCamilo <mac.pietri10@gmail.com>
2023-12-12 23:07:53 -0400

    grammar checker tried (almost but failed)

---
+++ lua/marco/plugins/grammar.lua
@@ -0,0 +1,6 @@
+return {
+  -- "vigoux/LanguageTool.nvim",
+  -- config = function()
+  --   vim.g.languagetool_server_jar = '$HOME/marco-andres/LanguageTool-6.3/languagetool-server.jar'
+  -- end,
+}
