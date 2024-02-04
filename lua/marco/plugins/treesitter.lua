df932699b5a7ba18c919fbfdf9df19b4f50d11b7
MarcoCamilo <mac.pietri10@gmail.com>
2024-02-04 21:19:31 +0100

    fully functioning quarto!

--- lua/marco/plugins/treesitter.lua
+++ lua/marco/plugins/treesitter.lua
@@ -1,47 +1,82 @@
-return 	{
+return {
   "nvim-treesitter/nvim-treesitter",
   tag = nil,
   branch = "master",
-  run = ":TSUpdate",
-  opts = {
-    ensure_installed = {
-      "r",
-      "python",
-      "markdown",
-      "markdown_inline",
-      "julia",
-      "bash",
-      "yaml",
-      "lua",
-      "vim",
-      "query",
-      "vimdoc",
-      "latex",
-      "html",
-      "css",
-      "dot",
-    },
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-      -- optional (with quarto-vim extension and pandoc-syntax)
-      -- additional_vim_regex_highlighting = { 'markdown' },
-
-      -- note: the vim regex based highlighting from
-      -- quarto-vim / vim-pandoc sets the wrong comment character
-      -- for some sections where there is `$` math.
-    },
-    indent = {
-      enable = true,
-    },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "gnn",
-        node_incremental = "grn",
-        scope_incremental = "grc",
-        node_decremental = "grm",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "r",
+        "python",
+        "markdown",
+        "markdown_inline",
+        "julia",
+        "bash",
+        "yaml",
+        "lua",
+        "vim",
+        "query",
+        "vimdoc",
+        "latex",
+        "html",
+        "css",
+        "dot"
       },
-    },
-  },
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+        -- optional (with quarto-vim extension and pandoc-syntax)
+        -- additional_vim_regex_highlighting = { 'markdown' },
+
+        -- note: the vim regex based highlighting from
+        -- quarto-vim / vim-pandoc sets the wrong comment character
+        -- for some sections where there is `$` math.
+      },
+      indent = {
+        enable = true,
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "gnn",
+          node_incremental = "grn",
+          scope_incremental = "grc",
+          node_decremental = "grm",
+        },
+      },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+          },
+        },
+        move = {
+          enable = true,
+          set_jumps = true, -- whether to set jumps in the jumplist
+          goto_next_start = {
+            ["]m"] = "@function.outer",
+            ["]]"] = "@class.inner",
+          },
+          goto_next_end = {
+            ["]M"] = "@function.outer",
+            ["]["] = "@class.outer",
+          },
+          goto_previous_start = {
+            ["[m"] = "@function.outer",
+            ["[["] = "@class.inner",
+          },
+          goto_previous_end = {
+            ["[M"] = "@function.outer",
+            ["[]"] = "@class.outer",
+          },
+        },
+      },
+    })
+  end,
 }
