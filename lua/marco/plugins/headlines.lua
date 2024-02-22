b0f4211accfe272d4a03d8469d3f67712434d4b7
MarcoCamilo <mac.pietri10@gmail.com>
2024-02-22 03:41:01 +0100

    headlines modified

--- lua/marco/plugins/headlines.lua
+++ lua/marco/plugins/headlines.lua
@@ -1,18 +1,32 @@
 return {
-  "lukas-reineke/headlines.nvim",
-  dependencies = "nvim-treesitter/nvim-treesitter",
-  config = function()
-    require("headlines").setup({
-      quarto = {
-        query = vim.treesitter.query.parse(
-          "markdown",
-          [[
+	"lukas-reineke/headlines.nvim",
+	dependencies = "nvim-treesitter/nvim-treesitter",
+	config = function()
+		local visual = {
+			bullets = { "◉", "○", "✸", "✿" },
+			codeblock_highlight = "CodeBlock",
+			dash_highlight = "Dash",
+			dash_string = "-",
+			quote_highlight = "Quote",
+			quote_string = "┃",
+			fat_headlines = true,
+			fat_headline_upper_string = "▃",
+			fat_headline_lower_string = "▀",
+		}
+		require("headlines").setup({
+			quarto = {
+				query = vim.treesitter.query.parse(
+					"markdown",
+					[[
               (fenced_code_block) @codeblock
           ]]
-        ),
-        codeblock_highlight = "CodeBlock",
-        treesitter_language = "markdown",
-      },
-    })
-  end,
+				),
+				codeblock_highlight = "CodeBlock",
+				treesitter_language = "markdown",
+        visual
+			},
+			markdown = visual,
+			rmd = visual,
+		})
+	end,
 }
