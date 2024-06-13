3d2b8b549d27dd17b432c220c9d71428125b87e5
Marco-Andrés Camilo-Pietri <72537451+marcocamilo@users.noreply.github.com>
2024-06-13 05:43:50 +0200

    neovim 10 + textobjects class added

--- lua/marco/plugins/textobjects.lua
+++ lua/marco/plugins/textobjects.lua
@@ -11,7 +11,8 @@ return {
           goto_next_start = {
             ["]f"] = { query = "@call.outer", desc = "Next function call start" },
             ["]m"] = { query = "@function.outer", desc = "Next method/function def start" },
-            ["]]"] = { query = "@codechunk.outer", desc = "Next class start" },
+            ["]]"] = { query = "@codechunk.outer", desc = "Next code chunk start" },
+            ["]c"] = { query = "@class.outer", desc = "Next class start" },
             ["]i"] = { query = "@conditional.outer", desc = "Next conditional start" },
             ["]l"] = { query = "@loop.outer", desc = "Next loop start" },
           },
@@ -25,7 +26,8 @@ return {
           goto_previous_start = {
             ["[f"] = { query = "@call.outer", desc = "Prev function call start" },
             ["[m"] = { query = "@function.outer", desc = "Prev method/function def start" },
-            ["[["] = { query = "@codechunk.outer", desc = "Prev class start" },
+            ["[["] = { query = "@codechunk.outer", desc = "Prev code chunk start" },
+            ["[c"] = { query = "@class.outer", desc = "Prev class start" },
             ["[i"] = { query = "@conditional.outer", desc = "Prev conditional start" },
             ["[l"] = { query = "@loop.outer", desc = "Prev loop start" },
           },
