6fd269d2cbf34dafaa70a1e5b2469cd931a445e0
Marco-Andrés Camilo-Pietri <72537451+marcocamilo@users.noreply.github.com>
2024-06-24 17:54:48 +0200

    ejs highlighter added

--- lua/marco/plugins/treesitter.lua
+++ lua/marco/plugins/treesitter.lua
@@ -1,57 +1,62 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  tag = nil,
-  branch = "master",
-  build = ":TSUpdate",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-  },
-  config = function()
-    local treesitter = require("nvim-treesitter.configs")
-    treesitter.setup({
-      ensure_installed = {
-        "bash",
-        "css",
-        "dot",
-        "gitignore",
-        "html",
-        "javascript",
-        "json",
-        "julia",
-        "latex",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "org",
-        "python",
-        "query",
-        "r",
-        "toml",
-        "vim",
-        "vimdoc",
-        "yaml",
-      },
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-        -- optional (with quarto-vim extension and pandoc-syntax)
-        -- additional_vim_regex_highlighting = { 'markdown' },
-
-        -- note: the vim regex based highlighting from
-        -- quarto-vim / vim-pandoc sets the wrong comment character
-        -- for some sections where there is `$` math.
-      },
-      indent = { enable = true },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
+  { 
+    "nvim-treesitter/nvim-treesitter",
+    tag = nil,
+    branch = "master",
+    build = ":TSUpdate",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    config = function()
+      local treesitter = require("nvim-treesitter.configs")
+      treesitter.setup({
+        ensure_installed = {
+          "bash",
+          "css",
+          "dot",
+          "gitignore",
+          "html",
+          "javascript",
+          "json",
+          "julia",
+          "latex",
+          "lua",
+          "markdown",
+          "markdown_inline",
+          "org",
+          "python",
+          "query",
+          "r",
+          "toml",
+          "vim",
+          "vimdoc",
+          "yaml",
         },
-      },
-    })
-  end,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+          -- optional (with quarto-vim extension and pandoc-syntax)
+          -- additional_vim_regex_highlighting = { 'markdown' },
+
+          -- note: the vim regex based highlighting from
+          -- quarto-vim / vim-pandoc sets the wrong comment character
+          -- for some sections where there is `$` math.
+        },
+        indent = { enable = true },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-space>",
+            node_incremental = "<C-space>",
+            scope_incremental = false,
+            node_decremental = "<bs>",
+          },
+        },
+      })
+    end, 
+  },
+  {
+    "nikvdp/ejs-syntax",
+  },
 }
