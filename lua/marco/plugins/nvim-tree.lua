1a4c9a5e982c36214f542c98e5b9323aa2cd0118
Marco-Andrés Camilo-Pietri <72537451+marcocamilo@users.noreply.github.com>
2024-07-07 00:25:50 +0200

    project.nvim removed + nvim-tree icon mod

--- lua/marco/plugins/nvim-tree.lua
+++ lua/marco/plugins/nvim-tree.lua
@@ -50,7 +50,7 @@ return {
         enable = true,
         show_on_dirs = true,
         icons = {
-          hint = "",
+          hint = "󰠠",
           info = "",
           warning = "",
           error = "",
