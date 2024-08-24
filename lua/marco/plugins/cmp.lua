1b393a332884619f662c17ccdb1413117ce35644
Marco-Andrés Camilo-Pietri <72537451+marcocamilo@users.noreply.github.com>
2024-08-24 23:21:03 +0200

    lean cmp

--- lua/marco/plugins/cmp.lua
+++ lua/marco/plugins/cmp.lua
@@ -3,16 +3,12 @@ return {
 	branch = "main",
 	dependencies = {
 		{ "hrsh7th/cmp-nvim-lsp" },
-		{ "hrsh7th/cmp-nvim-lsp-signature-help" },
 		{ "hrsh7th/cmp-buffer" },
 		{ "hrsh7th/cmp-path" },
-		{ "hrsh7th/cmp-calc" },
 		{ "hrsh7th/cmp-emoji" },
 		{ "saadparwaiz1/cmp_luasnip" },
-		{ "f3fora/cmp-spell" },
 		{ "ray-x/cmp-treesitter" },
 		{ "kdheepak/cmp-latex-symbols" },
-		{ "jmbuhr/cmp-pandoc-references" },
 		{ "L3MON4D3/LuaSnip" },
 		{ "rafamadriz/friendly-snippets" },
 		{ "onsails/lspkind-nvim" },
@@ -66,62 +62,6 @@ return {
 			},
 			autocomplete = false,
 			formatting = {
-				-- CONFIG 1: SOURCE NAME
-				-- format = function(entry, vim_item)
-				-- 	local icons = {
-				-- 		otter = "[🦦]",
-				-- 		copilot = "[]",
-				-- 		luasnip = "[snip]",
-				-- 		nvim_lsp = "[LSP]",
-				-- 		buffer = "[buf]",
-				-- 		path = "[path]",
-				-- 		spell = "[spell]",
-				-- 		pandoc_references = "[ref]",
-				-- 		tags = "[tag]",
-				-- 		treesitter = "[TS]",
-				-- 		calc = "[calc]",
-				-- 		latex_symbols = "[tex]",
-				-- 		emoji = "[emoji]",
-				-- 	}
-				-- 	if entry.source.name == "nvim_lsp" then
-				-- 		vim_item.menu =
-				-- 			string.format("%s %s", icons[entry.source.name], entry.source.source.client.name)
-				-- 	else
-				-- 		vim_item.menu = icons[entry.source.name]
-				-- 	end
-				-- 	return vim_item
-				-- end,
-				-- CONFIG 2: ORIGINAL
-				-- format = lspkind.cmp_format({
-				--        mode = "text_symbol",
-				-- 	menu = {
-				-- 		otter = "[🦦]",
-				-- 		nvim_lsp = "[LSP]",
-				-- 		luasnip = "[snip]",
-				-- 		buffer = "[buf]",
-				-- 		path = "[path]",
-				-- 		spell = "[spell]",
-				-- 		pandoc_references = "[ref]",
-				-- 		tags = "[tag]",
-				-- 		treesitter = "[TS]",
-				-- 		calc = "[calc]",
-				-- 		latex_symbols = "[tex]",
-				-- 		emoji = "[emoji]",
-				-- 	},
-				-- }),
-				-- CONFIG 3: devcons
-				-- format = function(entry, vim_item)
-				--   if vim.tbl_contains({ 'path' }, entry.source.name) then
-				--     local icon, hl_group = require('nvim-web-devicons').get_icon(entry:get_completion_item().label)
-				--     if icon then
-				--       vim_item.kind = icon
-				--       vim_item.kind_hl_group = hl_group
-				--       return vim_item
-				--     end
-				--   end
-				--   return require('lspkind').cmp_format({ with_text = false })(entry, vim_item)
-				-- end
-				-- CONFIG 5: custom
 				fields = { "abbr", "kind", "menu" },
 				format = function(entry, vim_item)
 					local menu
@@ -133,11 +73,8 @@ return {
 						luasnip = "[󰢱]",
 						buffer = "[󰹑]",
 						path = "[]",
-						spell = "[]",
-						pandoc_references = "[󰏛]",
 						tags = "[󰏢]",
 						treesitter = "[]",
-						calc = "[calc]",
 						latex_symbols = "[]",
 						emoji = "[󰱨]",
 					}
@@ -171,22 +108,17 @@ return {
 					},
 				},
 				{ name = "nvim_lsp" },
-				{ name = "nvim_lsp_signature_help" },
 				{ name = "luasnip" },
-				{ name = "pandoc_references" },
 				{ name = "buffer" },
-				{ name = "spell" },
 				{ name = "treesitter" },
-				{ name = "calc" },
 				{ name = "latex_symbols" },
 				{ name = "emoji" },
 			},
 		})
 
-		-- for friendly snippets
+		-- Load friendly snippets
 		require("luasnip.loaders.from_vscode").lazy_load()
-		-- for custom snippets
-		require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snips" } })
+
 		-- link quarto and rmarkdown to markdown snippets
 		luasnip.filetype_extend("quarto", { "markdown" })
 		luasnip.filetype_extend("rmarkdown", { "markdown" })
