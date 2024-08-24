e833b29fb197a5541c41d12e6fac268d77d143e4
Marco-Andrés Camilo-Pietri <72537451+marcocamilo@users.noreply.github.com>
2024-08-24 23:55:27 +0200

    formatting cleaned

--- lua/marco/plugins/formatting.lua
+++ lua/marco/plugins/formatting.lua
@@ -18,11 +18,6 @@ return {
 				latex = { "latexindent" },
 				yaml = { "prettier" },
 			},
-			-- format_on_save = {
-			-- 	lsp_fallback = true,
-			-- 	async = false,
-			-- 	timeout_ms = 500,
-			-- },
 		})
 
 		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
