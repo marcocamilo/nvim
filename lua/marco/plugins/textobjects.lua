e722ac0e98e6f8e527bc5b1059dad8afd502352a
Marco-Andrés Camilo-Pietri <72537451+marcocamilo@users.noreply.github.com>
2024-09-08 21:51:04 +0200

    text objects: goto next/prev end added

--- lua/marco/plugins/textobjects.lua
+++ lua/marco/plugins/textobjects.lua
@@ -11,7 +11,7 @@ return {
           goto_next_start = {
             ["]f"] = { query = "@call.outer", desc = "Next function call start" },
             ["]m"] = { query = "@function.outer", desc = "Next definition start" },
-            ["]]"] = { query = "@codechunk.outer", desc = "Next code chunk start" },
+            ["]b"] = { query = "@codechunk.outer", desc = "Next code chunk start" },
             ["]c"] = { query = "@class.outer", desc = "Next class start" },
             ["]i"] = { query = "@conditional.outer", desc = "Next conditional start" },
             ["]l"] = { query = "@loop.outer", desc = "Next loop start" },
@@ -19,10 +19,26 @@ return {
           goto_previous_start = {
             ["[f"] = { query = "@call.outer", desc = "Prev function call start" },
             ["[m"] = { query = "@function.outer", desc = "Prev method/function def start" },
-            ["[["] = { query = "@codechunk.outer", desc = "Prev code chunk start" },
+            ["[b"] = { query = "@codechunk.outer", desc = "Prev code chunk start" },
             ["[c"] = { query = "@class.outer", desc = "Prev class start" },
             ["[i"] = { query = "@conditional.outer", desc = "Prev conditional start" },
             ["[l"] = { query = "@loop.outer", desc = "Prev loop start" },
+          },
+          goto_next_end = {
+            ["]F"] = { query = "@call.outer", desc = "Next function call end" },
+            ["]M"] = { query = "@function.outer", desc = "Next definition end" },
+            ["]B"] = { query = "@codechunk.outer", desc = "Next code chunk end" },
+            ["]C"] = { query = "@class.outer", desc = "Next class end" },
+            ["]I"] = { query = "@conditional.outer", desc = "Next conditional end" },
+            ["]L"] = { query = "@loop.outer", desc = "Next loop end" },
+          },
+          goto_previous_end = {
+            ["[F"] = { query = "@call.outer", desc = "Prev function call end" },
+            ["[M"] = { query = "@function.outer", desc = "Prev method/function def end" },
+            ["[B"] = { query = "@codechunk.outer", desc = "Prev code chunk end" },
+            ["[C"] = { query = "@class.outer", desc = "Prev class end" },
+            ["[I"] = { query = "@conditional.outer", desc = "Prev conditional end" },
+            ["[L"] = { query = "@loop.outer", desc = "Prev loop end" },
           },
         },
         select = {
